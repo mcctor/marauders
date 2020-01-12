dba560f64e339a028f705633e0d3e43a19899d9f
mcctor <markvictormwanga@gmail.com>
2020-01-12 12:21:57 +0300

    Add various middleware functions to mux.Router.

--- http/server.go
+++ http/server.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -9,13 +10,17 @@ import (
 
 var Server *http.Server
 var Router *mux.Router
+var ServerAddr string
 
 func init() {
+	// create and apply middleware to root router
 	Router = mux.NewRouter()
+	Router.Use(ApplyContentTypeCollectionsJson, ApplyGzipCompression)
 	Server = &http.Server{
 		Addr:         "localhost:8080",
 		Handler:      Router,
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
 	}
+	ServerAddr = fmt.Sprintf("http://%s", Server.Addr)
 }
